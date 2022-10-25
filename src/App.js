81475c03d4589d1ce0a401aacb7d2295a3882c4b
deceser <aufhaummtobsc@gmail.com>
2022-10-25 17:48:29 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ function App() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
       <TextField id="standard-basic" label="Standard" variant="standard" />
     </Box>
   );
