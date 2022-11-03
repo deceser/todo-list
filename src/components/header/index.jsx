ff9fd968327b4e5755e040230223afd708d6bb53
deceser <aufhaummtobsc@gmail.com>
2022-11-03 11:18:33 +0200

    btn

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import ModeNightTwoToneIcon from "@mui/icons-material/ModeNightTwoTone";
+import FlareTwoToneIcon from "@mui/icons-material/FlareTwoTone";
 import Brightness3Icon from "@mui/icons-material/Brightness3";
 import Typography from "@mui/material/Typography";
-import { AppBar, Paper } from "@mui/material";
+import { AppBar } from "@mui/material";
 
-const Header = ({ clickTheme }) => {
+const Header = ({ clickTheme, light }) => {
   return (
     <AppBar elevation={0}>
       <Toolbar>
@@ -19,7 +19,7 @@ const Header = ({ clickTheme }) => {
         </Box>
         <Box>
           <IconButton onClick={clickTheme} variant="dashed">
-            <Brightness3Icon />
+            {light ? <Brightness3Icon /> : <FlareTwoToneIcon />}
           </IconButton>
         </Box>
       </Toolbar>
